72642be3fb290dbcf527628217b97cf78eda04d3
Diëggho <diegghooliver@gmail.com>
2019-12-28 23:23:42 -0300

    Add files via upload

---
+++ arc-dwm/dmenu/config.def.h
@@ -0,0 +1,24 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"sans:size=9"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#2B2E37" },
+	[SchemeSel] = { "#eeeeee", "#4084D6" },
+	[SchemeOut] = { "#000000", "#2B5A91" },
+	[SchemeMid] = { "#eeeeee", "#244A78" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";

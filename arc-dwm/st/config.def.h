ea9e0ab5581385c78bd8a05100bf4584a0ab71ec
Diëggho <diegghooliver@gmail.com>
2020-02-09 09:59:30 -0300

    Update config.def.h

--- arc-dwm/st/config.def.h
+++ arc-dwm/st/config.def.h
@@ -140,7 +140,7 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 89;
+static unsigned int cols = 92;
 static unsigned int rows = 29;
 
 /*

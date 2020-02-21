12b59adebbb1819b28157369c7772d91eb8ba63c
Diëggho <diegghooliver@gmail.com>
2020-02-21 13:22:52 -0300

    Add files via upload

--- arc-dwm/slock/config.def.h
+++ arc-dwm/slock/config.def.h
@@ -3,10 +3,19 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INIT] =   "#383C4A",     /* after initialization */
+	[INPUT] =  "#2D5D96",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "-misc-dejavu serif-bold-r-normal--0-0-0-0-p-0-iso8859-16";

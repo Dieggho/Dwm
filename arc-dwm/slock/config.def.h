851f62542eaf77ff446dc75d90aa7fcb06574772
Diëggho <diegghooliver@gmail.com>
2020-02-26 13:18:21 -0300

    Add files via upload

--- arc-dwm/slock/config.def.h
+++ arc-dwm/slock/config.def.h
@@ -1,21 +1,12 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#000000",   /* after initialization */
-	[INPUT] =  "#2D5D96",   /* during input */
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#4277B5",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
-/* default message */
-static const char * message = "There is no knowledge that is not power";
-
-/* text color */
-static const char * text_color = "#ffffff";
-
-/* text size (must be a valid size) */
-static const char * font_name = "-misc-dejavu serif-bold-r-normal--0-0-0-0-p-0-iso8859-16";
